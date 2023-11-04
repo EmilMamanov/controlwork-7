d67f1e173bb2a45e5a9450ee9c534ac76674cf17
Mamanov Emil <emil.mamanov97@gmail.com>
2023-11-04 14:33:43 +0600

    added logic without components

--- controlwork-7/src/App.tsx
+++ controlwork-7/src/App.tsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
+import hamburgerImage from './assets/hamburger.png';
+import coffeeImage from './assets/coffee.png';
+import cheeseburgerImage from './assets/cheeseburger.png';
+import teaImage from './assets/tea.png';
+import friesImage from './assets/fries.png';
+import colaImage from './assets/cola.png';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [order, setOrder] = useState<{ [itemName: string]: number }>({});
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const addToOrder = (item: { name: string; price: number; image: string }) => {
+        const newOrder = { ...order };
+
+        if (newOrder[item.name] !== undefined) {
+            newOrder[item.name]++;
+        } else {
+            newOrder[item.name] = 1;
+        }
+
+        setOrder(newOrder);
+    };
+
+    const removeFromOrder = (itemName: string) => {
+        const newOrder = { ...order };
+
+        if (newOrder[itemName] !== undefined) {
+            if (newOrder[itemName] > 1) {
+                newOrder[itemName]--;
+            } else {
+                delete newOrder[itemName];
+            }
+
+            setOrder(newOrder);
+        }
+    };
+
+    const menuItems = [
+        { name: 'Hamburger', price: 80, image: hamburgerImage },
+        { name: 'Coffee', price: 70, image: coffeeImage },
+        { name: 'Cheeseburger', price: 90, image: cheeseburgerImage },
+        { name: 'Tea', price: 50, image: teaImage },
+        { name: 'Fries', price: 45, image: friesImage },
+        { name: 'Cola', price: 40, image: colaImage },
+    ];
+
+    const totalOrderPrice = () => {
+        return Object.keys(order).reduce((total, itemName) => {
+            const item = menuItems.find((menuitem) => menuitem.name === itemName);
+            if (item) {
+                return total + item.price * order[itemName];
+            }
+            return total;
+        }, 0);
+    };
+
+    return (
+        <div className="App">
+            <div className="OrderPanel">
+                <h2>Order</h2>
+                {Object.keys(order).length === 0 ? (
+                    <p>Order is empty! Please add some items!</p>
+                ) : (
+                    <div>
+                        <ul>
+                            {Object.keys(order).map((itemName, index) => (
+                                <li key={index}>
+                                    <img src={menuItems.find((item) => item.name === itemName)?.image} alt={itemName} />
+                                    {itemName} - {menuItems.find((item) => item.name === itemName)?.price} KGS ({order[itemName]})
+                                    <button onClick={() => removeFromOrder(itemName)} className="remove-btn">х</button>
+                                </li>
+                            ))}
+                        </ul>
+                        <p>Total price: {totalOrderPrice()} KGS</p>
+                    </div>
+                )}
+            </div>
+            <div className="AddItemsPanel">
+                <h2>Add items</h2>
+                <ul>
+                    {menuItems.map((item, index) => (
+                        <li key={index}>
+                            <img src={item.image} alt={item.name} />
+                            {item.name} - {item.price} KGS
+                            <button onClick={() => addToOrder(item)} className="add-to-order-btn">+</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
